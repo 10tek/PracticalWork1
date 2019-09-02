26d2e80d0554e0724bf953710f6c9d557dbef7d7
10tekinvo@gmail.com <10tekinvo@gmail.com>
2019-09-02 18:19:23 +0600

    Выводится имя и фамилия, а так же программа для подсчета суммы вводимых чисел.

---
+++ PW1/PW1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PracticalWork1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Oralbayev Galymzhan!");
+
+            Console.WriteLine("Введите количество чисел для подсчета суммы");
+            var count = Convert.ToInt32(Console.ReadLine());
+            var sum = 0;
+            while (count > 0)
+            {
+                sum += Convert.ToInt32(Console.ReadLine());
+                count--;
+            }
+            Console.WriteLine($"Сумма чисел: {sum}");
+
+            Console.ReadKey();
+        }
+    }
+}
